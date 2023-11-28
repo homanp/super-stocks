4575ce1e17bf646b302828e3d9a2b6ba101cb787
Ismail Pelaseyed <homanp@gmail.com>
2023-11-28 23:26:23 +0100

    Fix dark-mode

--- app/layout.tsx
+++ app/layout.tsx
@@ -19,7 +19,6 @@ export default function RootLayout({
         <ThemeProvider
           attribute="class"
           defaultTheme="dark"
-          enableSystem
           disableTransitionOnChange
         >
           {children}
